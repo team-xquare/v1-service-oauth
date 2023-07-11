be908b4ed9ce0af4929830f0a4f5e612129be97e
rlaisqls <rlaisqls@gmail.com>
2023-07-11 16:14:05 +0900

    feat: UserInfoDto 컬럼 추가

--- src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/user/repository/dtos/UserInformationDto.kt
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/user/repository/dtos/UserInformationDto.kt
@@ -1,9 +1,17 @@
 package com.example.v1oauthauthorizationservice.infrastructure.user.repository.dtos
 
+import java.time.LocalDate
 import java.util.UUID
 
 data class UserInformationDto(
-    val name: String,
+
     val id: UUID,
+    val accountId: String,
+    val name: String,
+    val birthDay: LocalDate,
+    val grade: Int,
+    val classNum: Int,
+    val num: Int,
+    val profileFileName: String?,
     val password: String
 )
