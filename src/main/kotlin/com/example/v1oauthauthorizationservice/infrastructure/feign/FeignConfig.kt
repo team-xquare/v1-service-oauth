e34fd2e7b913cfd32ac325b90aae23f0c6c2f61e
rlaisqls <rlaisqls@gmail.com>
2023-07-11 23:42:57 +0900

    chore: feignDecoder

--- src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/feign/FeignConfig.kt
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/feign/FeignConfig.kt
@@ -1,6 +1,12 @@
 package com.example.v1oauthauthorizationservice.infrastructure.feign
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.type.TypeFactory
+import feign.Response
+import feign.Util
+import feign.codec.Decoder
 import feign.codec.ErrorDecoder
+import java.lang.reflect.Type
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.cloud.openfeign.EnableFeignClients
@@ -8,14 +14,25 @@ import org.springframework.cloud.openfeign.FeignAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-
 @EnableFeignClients(basePackages = ["com.example.v1oauthauthorizationservice.infrastructure.feign"])
 @ImportAutoConfiguration(FeignAutoConfiguration::class)
 @Configuration
-class FeignConfig {
+class FeignConfig(
+    private val objectMapper: ObjectMapper
+) {
     @Bean
     @ConditionalOnMissingBean(value = [ErrorDecoder::class])
     fun commonFeignErrorDecoder(): FeignClientErrorDecoder {
         return FeignClientErrorDecoder()
     }
+
+    @Bean
+    fun feignDecoder() =
+        Decoder { response: Response, type: Type ->
+            val bodyStr = Util.toString(
+                response.body().asReader(Util.UTF_8)
+            )
+            val javaType = TypeFactory.defaultInstance().constructType(type)
+            objectMapper.readValue(bodyStr, javaType)
+        }
 }
