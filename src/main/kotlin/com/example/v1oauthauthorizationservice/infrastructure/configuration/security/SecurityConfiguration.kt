ca7ea840128e2f69b873c0159ab51e0ad8f0f621
rlaisqls <rlaisqls@gmail.com>
2023-07-11 16:10:21 +0900

    refactor: SecurityConfig 분리

---
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/configuration/security/SecurityConfiguration.kt
@@ -0,0 +1,65 @@
+package com.example.v1oauthauthorizationservice.infrastructure.configuration.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+
+@EnableWebSecurity(debug = true)
+@Configuration
+class SecurityConfiguration(
+    private val objectMapper: ObjectMapper,
+    private val authConfiguration: AuthenticationConfiguration
+) {
+
+    @Bean
+    fun authenticationManager(): AuthenticationManager = authConfiguration.authenticationManager
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    fun securityFilterChain(
+        httpSecurity: HttpSecurity
+    ): SecurityFilterChain {
+
+        httpSecurity
+            .cors().disable()
+            .csrf().disable()
+
+        httpSecurity
+            .formLogin().disable()
+            .httpBasic().disable()
+            .csrf().disable()
+            .headers().frameOptions().disable().and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+        httpSecurity
+            .authorizeHttpRequests { authorize ->
+                authorize
+                    .requestMatchers(HttpMethod.POST, "/oauth2/token").permitAll()
+                    .requestMatchers("/v1/users/login").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/jwk").permitAll()
+                    .requestMatchers("/tokens").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/oauth2/authorize").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/oauth2/client").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/oauth2/userinfo").permitAll()
+                    .anyRequest().permitAll()
+            }
+
+        return httpSecurity.build()
+    }
+}
