5d8f88445f9c475b38ac512ccdbaa1482ecdb46c
김은빈 <rlaisqls@gmail.com>
2023-08-02 15:28:46 +0900

    update :: cloud config 연결

--- src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/feign/client/UserClient.kt
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/feign/client/UserClient.kt
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name = "UserClient", url = "\${service.scheme}://\${service.user.host}")
+@FeignClient(name = "userClient", url = "${service.scheme}://${service.user.host}")
 interface UserClient {
 
     @GetMapping("/users/id/{userId}")
