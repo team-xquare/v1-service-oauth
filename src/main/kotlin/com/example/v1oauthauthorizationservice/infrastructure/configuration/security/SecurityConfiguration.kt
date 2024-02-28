ba05210f1a4b3f84c546a678b25591537df6c73a
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-29 07:09:56 +0900

    update :: 오류 수정

--- src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/configuration/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/configuration/security/SecurityConfiguration.kt
@@ -52,6 +52,7 @@ class SecurityConfiguration(
                     .requestMatchers(HttpMethod.POST, "/oauth2/token").permitAll()
                     .requestMatchers(HttpMethod.GET, "/jwk").permitAll()
                     .requestMatchers(HttpMethod.GET, "/oauth2/authorize").authenticated()
+                    .requestMatchers(HttpMethod.GET, "/oauth2/userinfo").authenticated()
                     .anyRequest().authenticated()
             }
 
