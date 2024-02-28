387652c59dc53e0e79dd63b35dbd3bb28575f70a
4mjeo <4mj.eo@dsm.hs.kr>
2024-02-29 03:08:07 +0900

    update :: 권한 제한

--- src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/configuration/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/configuration/security/SecurityConfiguration.kt
@@ -1,7 +1,6 @@
 package com.example.v1oauthauthorizationservice.infrastructure.configuration.security
 
 import com.example.v1oauthauthorizationservice.infrastructure.configuration.AuthenticationFilter
-import com.example.v1oauthauthorizationservice.infrastructure.user.security.AuthDetailsService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
@@ -18,8 +17,7 @@ import org.springframework.security.web.access.intercept.AuthorizationFilter
 @EnableWebSecurity(debug = true)
 @Configuration
 class SecurityConfiguration(
-    private val authenticationFilter: AuthenticationFilter,
-        private val authDetailsService: AuthDetailsService
+    private val authenticationFilter: AuthenticationFilter
 ) {
 
     @Bean
@@ -51,10 +49,10 @@ class SecurityConfiguration(
                     .requestMatchers(HttpMethod.POST, "/oauth2/client").authenticated()
                     .requestMatchers(HttpMethod.PATCH, "/oauth2/client/{client-id}").authenticated()
                     .requestMatchers(HttpMethod.GET, "/oauth2/client/{client-id}/secret").authenticated()
-                    .requestMatchers(HttpMethod.POST, "/oauth2/token").authenticated()
+                    .requestMatchers(HttpMethod.POST, "/oauth2/token").permitAll()
                     .requestMatchers(HttpMethod.GET, "/jwk").permitAll()
                     .requestMatchers(HttpMethod.GET, "/oauth2/authorize").authenticated()
-                    .requestMatchers(HttpMethod.GET, "/oauth2/userinfo").authenticated()
+                    .requestMatchers(HttpMethod.GET, "/oauth2/userinfo").permitAll()
                     .anyRequest().authenticated()
             }
 
