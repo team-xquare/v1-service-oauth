c6c4491f35a6a3c6f5022e601bbcb90e3ee98ef5
4mjeo <4mj.eo@dsm.hs.kr>
2024-03-09 16:03:05 +0900

    chore :: add jwt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,6 +59,10 @@ dependencies {
 
     // secondary cache
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    //jwt
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+
 }
 
 dependencyManagement {
