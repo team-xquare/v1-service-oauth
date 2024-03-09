1ea20ab4c03606f9aa439c99b9ad503ead86b0a3
4mjeo <4mj.eo@dsm.hs.kr>
2024-03-09 17:07:47 +0900

    update :: update default url

--- src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/user/presentation/UserController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*
 
 @Validated
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/oauth2/user")
 class UserController(
     private val userService: UserService
 ) {
