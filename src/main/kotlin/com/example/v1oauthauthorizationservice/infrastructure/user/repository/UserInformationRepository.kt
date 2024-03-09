a3d7f5784170cce39e605eb99dbc5e133e511296
4mjeo <4mj.eo@dsm.hs.kr>
2024-03-09 12:25:35 +0900

    update :: userRepository 생성

--- src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/user/repository/UserInformationRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.v1oauthauthorizationservice.infrastructure.user.repository
-
-import com.example.v1oauthauthorizationservice.infrastructure.user.repository.dtos.UserInformationDto
-import java.util.UUID
-
-interface UserInformationRepository {
-    fun findUserById(userId: UUID): UserInformationDto
-    fun findUserByAccountId(accountId: String): UserInformationDto
-}
---
+++ src/main/kotlin/com/example/v1oauthauthorizationservice/infrastructure/user/repository/UserRepository.kt
@@ -0,0 +1,14 @@
+package com.example.v1oauthauthorizationservice.infrastructure.user.repository
+
+import com.example.v1oauthauthorizationservice.infrastructure.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface UserRepository: JpaRepository<User, UUID?> {
+
+    fun findByAccountId(accountId: String): User?
+
+    fun findUserById(userId: UUID): User?
+}
